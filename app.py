445c0c74eb586781c82d21678880748aa9f9924d
JOSE CARLOS VEGA MORON <vegamoronjose98@gmail.com>
2024-11-08 19:11:12 -0500

    Version final

--- app.py
+++ app.py
@@ -1,49 +1,52 @@
 import streamlit as st
 import pandas as pd
-from utils import load_model, preprocess_data, predict_anomalies
+from utils import load_model, preprocess_data, predict_anomalies, load_data_from_pkl
+
 st.set_page_config(page_icon="🤡", page_title="Trabajo Final")
 st.image("logob_m_EPG.png", width=200)
 st.title("Detección de anomalías para clientes en el sector financiero")
 col1, col2 = st.columns([5, 5])
 
-with col1:
-    st.subheader("Algoritmo Isolation Forest para Detección de Fraude")
-    st.info("""
-    El algoritmo **Isolation Forest** es un método de aprendizaje automático utilizado para la detección de anomalías.
-    Se basa en el principio de que los puntos de datos anómalos (en este caso, posibles casos de fraude) son más fáciles de 
-    "aislar" o separar que los puntos normales. En el contexto financiero, Isolation Forest ayuda a identificar patrones 
-    sospechosos en las cuentas de los clientes, lo que puede indicar actividades fraudulentas.
-    
-    Este modelo divide los datos en árboles de aislamiento y mide la facilidad con la que un punto se aísla. Si un punto es 
-    aislado rápidamente, se clasifica como una posible anomalía, permitiendo así una detección temprana de posibles fraudes 
-    en las cuentas de los clientes.
-    """)
-with col2:
-    uploaded_model = st.file_uploader(
-        "Sube un archivo .pkl para el modelo de análisis", type='pkl',
-        key="1"
-    )
 
-    if uploaded_model is not None:
+st.subheader("Algoritmo Isolation Forest para Detección de Fraude")
+st.info("""
+El algoritmo **Isolation Forest** es un método de aprendizaje automático utilizado para la detección de anomalías.
+Se basa en el principio de que los puntos de datos anómalos (en este caso, posibles casos de fraude) son más fáciles de 
+"aislar" o separar que los puntos normales. En el contexto financiero, Isolation Forest ayuda a identificar patrones 
+sospechosos en las cuentas de los clientes, lo que puede indicar actividades fraudulentas.
 
-        model = load_model(uploaded_model)
+Este modelo divide los datos en árboles de aislamiento y mide la facilidad con la que un punto se aísla. Si un punto es 
+aislado rápidamente, se clasifica como una posible anomalía, permitiendo así una detección temprana de posibles fraudes 
+en las cuentas de los clientes.
+""")
+
+
+model_path = 'isolation_forest_model.pth'
+model = load_model(model_path)
+st.success("Modelo Isolation Forest cargado desde el archivo local.")
+
+data_file = st.file_uploader("Sube un archivo .pkl con datos de entrada", type="pkl", key="data_upload")
+if data_file is not None:
+    try:
         
-
-        data_file = st.file_uploader("Sube un archivo .csv de datos de transacciones", type="csv", key="2")
+        data = load_data_from_pkl(data_file)
         
-        if data_file is not None:
-    
-            data = pd.read_csv(data_file)
-            st.write("Datos cargados con éxito:", data.head())
+        
+        if isinstance(data, pd.DataFrame):
+            st.write("Datos cargados con éxito:", data)
             
-    
+            
             data_scaled = preprocess_data(data)
             anomalies = predict_anomalies(model, data_scaled)
             
-    
+            
             st.write("Resultados de la detección de fraude:")
             data['EsFraude'] = anomalies
             st.write(data[['EsFraude']])
             st.info(f"Total de transacciones anómalas detectadas: {sum(anomalies)}")
-    else:
-        st.info("Por favor, sube el modelo en formato .pkl.")
+        else:
+            st.error("El archivo .pkl no contiene un DataFrame válido.")
+    except Exception as e:
+        st.error(f"Ocurrió un error al cargar el archivo .pkl: {e}")
+else:
+    st.info("Por favor, sube el archivo de datos en formato .pkl.")
--- utils.py
+++ utils.py
@@ -1,19 +1,30 @@
-import joblib
+import torch
+import pickle
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
 
-MODEL_PATH = 'best_isolation_forest_model.pkl'
-
-def load_model():
-    model = joblib.load(MODEL_PATH)
+def load_model(model_path):
+    model = torch.load(model_path) 
     return model
 
 def preprocess_data(data):
+
+    imputer = SimpleImputer(strategy='median')
+    data_imputed = imputer.fit_transform(data)
+    
+
     scaler = StandardScaler()
-    data_scaled = scaler.fit_transform(data)
+    data_scaled = scaler.fit_transform(data_imputed)
     return data_scaled
 
 def predict_anomalies(model, data):
+
     predictions = model.predict(data)
-    anomalies = predictions == -1
+
+    anomalies = (predictions == -1).astype(int)
     return anomalies
+
+def load_data_from_pkl(file):
+    data = pickle.load(file)
+    return pd.DataFrame(data)
